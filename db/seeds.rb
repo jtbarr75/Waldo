fd2314d1290641278eb5e0e085051e90fffd851f
Jason Barr <Barr75@gmail.com>
2020-08-24 14:21:56 -0700

    add test waldo seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Character.create({ name: 'Waldo', xpos: 420, ypos: 69 })
