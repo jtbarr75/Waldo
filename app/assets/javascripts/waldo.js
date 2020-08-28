6d70151f8d73117d56c19473a755e7a53f9d164c
Jason Barr <Barr75@gmail.com>
2020-08-28 15:36:58 -0700

    select box uses relative position in image, accounts for scroll

--- app/assets/javascripts/waldo.js
+++ app/assets/javascripts/waldo.js
@@ -1,5 +1,6 @@
 ( function() {
-  document.onmousemove = updateMouseLocation; 
+  // document.onmousemove = updateMouseLocation;
+  document.onclick = selectBox;
 
   function updateMouseLocation(e) {
     let x = e.clientX;
@@ -7,5 +8,27 @@
 
     let mousePos =document.getElementById("mousePos");
     mousePos.innerText = `${x} ${y}`;
+  }
+
+  function selectBox(e) {
+    if (e.target.id !== "image") {
+      return;
+    }
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX + document.body.scrollLeft - rect.left;
+    let y = e.clientY + document.body.scrollTop - rect.top;
+
+    createSelectBox(x, y);
+
+    let mousePos = document.getElementById("mousePos");
+    mousePos.innerText += `, x: ${x} y: ${y}`;
+  }
+
+  function createSelectBox(x, y){
+    let box = document.createElement("div");
+    box.classList.add("select-box");
+    box.style.top = `${y- 30}px`;
+    box.style.left = `${x - 20}px`;
+    document.querySelector(".image-wrapper").appendChild(box);
   }
 })();
