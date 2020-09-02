1a0435abeb74c1e4ee6444201dc4f07fd13f33a3
Jason Barr <Barr75@gmail.com>
2020-09-01 17:03:07 -0700

    update url for submitting score

---
+++ app/assets/javascripts/timer.js
@@ -0,0 +1,56 @@
+const timer = ( function() {
+  let running = true;
+  const start = new Date().getTime();
+
+  let timerInterval = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = now - start;
+    const timer = document.getElementById("timer");
+    timer.innerText = formatTime(distance);
+    timer.dataset.time = distance;
+
+    if (!running) {
+      clearInterval(timerInterval);
+    }
+  }, 1000)
+
+  function stopTimer(){
+    running = false;
+  }
+
+  function formatTime(time) {
+    const minutes = Math.floor(time / (1000 * 60));
+    const seconds = Math.floor((time % (1000 * 60)) / 1000);
+    return `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+  }
+
+  return {
+    stopTimer,
+    formatTime
+  }
+})();
+
+function submitScore() {
+  event.preventDefault();
+  const time = document.getElementById("timer").dataset.time;
+  const name = document.getElementById("name").value;
+  const body = { name: name, time: time }
+  const url = "/scores";
+  const token = document.querySelector('meta[name="csrf-token"]').content;
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "X-CSRF-Token": token,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network response not ok.")
+    })
+    .then(console.log("Score created"))
+    .catch(error => {console.log(error.message, "Something bad happened")})
+}
